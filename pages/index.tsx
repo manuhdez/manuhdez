493d1c0819cf931bb97fe869252284309fa0e7e7
Manu Hdez <manu.hdez.dev@gmail.com>
2020-07-15 22:47:45 +0100

    minor update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import Head from 'next/head';
 import styles from './index.module.scss';
 
-const Home = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Manu Hdez</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Manu Hdez</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main>
-      <h1 className={styles.title}>Hello world</h1>
-    </main>
-  </div>
-);
-
-export default Home;
+      <main>
+        <h1 className={styles.title}>Hello world</h1>
+      </main>
+    </div>
+  );
+}
