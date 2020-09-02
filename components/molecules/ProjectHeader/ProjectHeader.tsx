dd8b00b16e4a7c62ef1bc5a08108bf9f9305aac2
Manu Hdez <manu.hdez.dev@gmail.com>
2020-09-02 08:03:44 +0100

    Conditionally render project url button

--- components/molecules/ProjectHeader/ProjectHeader.tsx
+++ components/molecules/ProjectHeader/ProjectHeader.tsx
@@ -13,29 +13,33 @@ export default function ProjectHeader(props: ProjectData) {
         <h1>{title}</h1>
         <p className={styles.short_description}>{short_description}</p>
         <div className={styles.actions}>
-          <a
-            className="btn btn-light"
-            href={url}
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            open project
-          </a>
-          <a
-            className="btn btn-dark"
-            href={code_url}
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            view source code
-          </a>
+          {url && (
+            <a
+              className="btn btn-light"
+              href={url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              open project
+            </a>
+          )}
+          {code_url && (
+            <a
+              className="btn btn-dark"
+              href={code_url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              view source code
+            </a>
+          )}
         </div>
       </header>
       <div className={styles.tags_container}>
         <h3>Skills</h3>
         <ul className={styles.tags_list}>
           {tags.map((tag) => (
-            <li key="tag" className="btn btn-light">
+            <li key={tag} className="btn btn-light">
               {tag}
             </li>
           ))}
