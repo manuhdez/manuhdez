535d115d47858d2f8ddf928e5dabab57ffaa8241
Manuel Hernández <manuel@secret-source.eu>
2021-02-12 18:41:56 +0000

    feat: cover update
    Update cover layout and spacing
    Update typography for dark mode
    Update bg color on tools section for dark mode

--- components/molecules/Hero/Hero.tsx
+++ components/molecules/Hero/Hero.tsx
@@ -3,8 +3,8 @@ import styles from './Hero.module.scss';
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Fullstack web developer with a taste for design</h1>
-      <p></p>
+      <h1>Software developer with a focus on user experience</h1>
+      <p>UI design / frontend / backend</p>
     </section>
   );
 }
