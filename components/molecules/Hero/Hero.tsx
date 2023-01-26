42ab7a2b189e83b44d3cafa69b7a937a031768d5
Manuel Hernández <hi@manuhdez.com>
2023-01-26 17:25:10 +0000

    feat: updated hero tagline

--- components/molecules/Hero/Hero.tsx
+++ components/molecules/Hero/Hero.tsx
@@ -1,10 +1,10 @@
-import styles from './Hero.module.scss';
+import styles from "./Hero.module.scss";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Software developer with a focus on user experience</h1>
-      <p>UI design / frontend / backend</p>
+      <h1>Hi! I'm Manuel Hernández</h1>
+      <h3>Software Engineer with 5+ years of experience in web development</h3>
     </section>
   );
 }
